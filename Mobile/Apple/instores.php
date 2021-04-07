7ff0f32c50aa2cc41f1edad541155dde59a57460
sudipkumardas <46398828+sudipkumardas@users.noreply.github.com>
2021-04-08 00:55:27 +0530

    Add files via upload

---
+++ Mobile/Apple/instores.php
@@ -0,0 +1,152 @@
+                <div class="in-store mt-4">
+                    <h3 class="text-center font-weight-bold mb-0 rounded" style="background-color: #dfe6e9;">In The Store</h3>
+                    <div class="d-flex flex-column" style="height:100%;">
+                        <div class="p-2 border rounded flex-fill store" style="background-image: url(light2.jpg)">
+                            <a href="https://www.amazon.in/gp/product/B07J3BSLNK/ref=as_li_tl?ie=UTF8&tag=skd0099-21&camp=3638&creative=24630&linkCode=as2&creativeASIN=B07J3BSLNK&linkId=f3965d2481972283203a760c33a9c414" target="_blank">
+                                <div class="d-flex align-items-center">
+                                    <div class="mr-1 mr-sm-1 mr-md-1 mr-lg-2">
+                                        <span class="s-image"><img src="apple-iphone-xs-max-1.jpeg"></span>
+                                    </div>
+                                    <div class="d-flex flex-column ml-1">
+                                        <div class="flex-fill">Apple iPhone Xs Max (256 GB)-Space Grey</div>
+                                        <div class="flex-fill">₹ 124,899</div>
+                                        <div class="flex-fill d-flex my-1">
+                                            <div class="mx-3 mx-sm-3 mx-md-1 mx-lg-4">
+                                                <span class="logo"><img src="../../../img/logo/amazon.jpg" alt="amazon-logo"></span>
+                                            </div>
+                                            <div class="ml-3 ml-sm-3 ml-md-1 ml-lg-4">
+                                                <span class="go-btn btn btn-warning d-inline">Buy Now</span>
+                                            </div>
+                                        </div>
+                                    </div>                                    
+                                </div>
+                            </a>
+                        </div>
+                        <div class="p-2 border rounded flex-fill store" style="background-image: url(light2.jpg)">
+                            <a href="https://www.amazon.in/gp/product/B07J2Z88Z9/ref=as_li_tl?ie=UTF8&tag=skd0099-21&camp=3638&creative=24630&linkCode=as2&creativeASIN=B07J2Z88Z9&linkId=76e398dea3b9a52f2267d148ea91258f" target="_blank">
+                                <div class="d-flex align-items-center">
+                                    <div class="mr-1 mr-sm-1 mr-md-1 mr-lg-2">
+                                        <span class="s-image"><img src="apple-iphone-xs-max-silver.jpeg"></span>
+                                    </div>
+                                    <div class="d-flex flex-column ml-1">
+                                        <div class="flex-fill">Apple iPhone Xs Max (64 GB)-Silver</div>
+                                        <div class="flex-fill">₹ 109,890</div>
+                                        <div class="flex-fill d-flex my-1">
+                                            <div class="mx-3 mx-sm-3 mx-md-1 mx-lg-4">
+                                                <span class="logo"><img src="../../../img/logo/amazon.jpg" alt="amazon-logo"></span>
+                                            </div>
+                                            <div class="ml-3 ml-sm-3 ml-md-1 ml-lg-4">
+                                                <span class="go-btn btn btn-warning d-inline">Buy Now</span>
+                                            </div>
+                                        </div>
+                                    </div>                                    
+                                </div>
+                            </a>
+                        </div>
+                        <div class="p-2 border rounded flex-fill store" style="background-image: url(light2.jpg)">
+                            <a href="https://www.amazon.in/gp/product/B07JVBP9B9/ref=as_li_tl?ie=UTF8&tag=skd0099-21&camp=3638&creative=24630&linkCode=as2&creativeASIN=B07JVBP9B9&linkId=a0ccef250f4f97f6b3e6d2348e0f048a" target="_blank">
+                                <div class="d-flex align-items-center">
+                                    <div class="mr-1 mr-sm-1 mr-md-1 mr-lg-2">
+                                        <span class="s-image"><img src="../iPhone-XS/apple-iphone-xs-1.jpeg"></span>
+                                    </div>
+                                    <div class="d-flex flex-column ml-1">
+                                        <div class="flex-fill">Apple iPhone Xs (256 GB)-Space Grey</div>
+                                        <div class="flex-fill">₹ 114,890</div>
+                                        <div class="flex-fill d-flex my-1">
+                                            <div class="mx-3 mx-sm-3 mx-md-1 mx-lg-4">
+                                                <span class="logo"><img src="../../../img/logo/amazon.jpg" alt="amazon-logo"></span>
+                                            </div>
+                                            <div class="ml-3 ml-sm-3 ml-md-1 ml-lg-4">
+                                                <span class="go-btn btn btn-warning d-inline">Buy Now</span>
+                                            </div>
+                                        </div>
+                                    </div>                                    
+                                </div>
+                            </a>
+                        </div>
+                        <div class="p-2 border rounded flex-fill store" style="background-image: url(light2.jpg)">
+                            <a href="https://www.amazon.in/gp/product/B07JVC6QRY/ref=as_li_tl?ie=UTF8&tag=skd0099-21&camp=3638&creative=24630&linkCode=as2&creativeASIN=B07JVC6QRY&linkId=6463695413a718f50de33df0bcfc7140" target="_blank">
+                                <div class="d-flex align-items-center">
+                                    <div class="mr-1 mr-sm-1 mr-md-1 mr-lg-2">
+                                        <span class="s-image"><img src="../iPhone-XS/apple-iphone-xs-1.jpeg"></span>
+                                    </div>
+                                    <div class="d-flex flex-column ml-1">
+                                        <div class="flex-fill">Apple iPhone Xs (64 GB)-Space Grey</div>
+                                        <div class="flex-fill">₹ 97,400</div>
+                                        <div class="flex-fill d-flex my-1">
+                                            <div class="mx-3 mx-sm-3 mx-md-1 mx-lg-4">
+                                                <span class="logo"><img src="../../../img/logo/amazon.jpg" alt="amazon-logo"></span>
+                                            </div>
+                                            <div class="ml-3 ml-sm-3 ml-md-1 ml-lg-4">
+                                                <span class="go-btn btn btn-warning d-inline">Buy Now</span>
+                                            </div>
+                                        </div>
+                                    </div>                                    
+                                </div>
+                            </a>
+                        </div>
+                        <div class="p-2 border rounded flex-fill store" style="background-image: url(light2.jpg)">
+                            <a href="https://www.amazon.in/gp/product/B07JG7DS1T/ref=as_li_tl?ie=UTF8&tag=skd0099-21&camp=3638&creative=24630&linkCode=as2&creativeASIN=B07JG7DS1T&linkId=045125b8874bc248246ad04b053b3351" target="_blank">
+                                <div class="d-flex align-items-center">
+                                    <div class="mr-1 mr-sm-1 mr-md-1 mr-lg-2">
+                                        <span class="s-image"><img src="../iPhone-XR/apple-iphone-xr-1.jpeg"></span>
+                                    </div>
+                                    <div class="d-flex flex-column ml-1">
+                                        <div class="flex-fill">Apple iPhone XR (128 GB)-Black</div>
+                                        <div class="flex-fill">₹ 64,900</div>
+                                        <div class="flex-fill d-flex my-1">
+                                            <div class="mx-3 mx-sm-3 mx-md-1 mx-lg-4">
+                                                <span class="logo"><img src="../../../img/logo/amazon.jpg" alt="amazon-logo"></span>
+                                            </div>
+                                            <div class="ml-3 ml-sm-3 ml-md-1 ml-lg-4">
+                                                <span class="go-btn btn btn-warning d-inline">Buy Now</span>
+                                            </div>
+                                        </div>
+                                    </div>                                    
+                                </div>
+                            </a>
+                        </div>
+                        <div class="p-2 border rounded flex-fill store" style="background-image: url(light2.jpg)">
+                            <a href="https://www.amazon.in/gp/product/B07JGXM9WN/ref=as_li_tl?ie=UTF8&tag=skd0099-21&camp=3638&creative=24630&linkCode=as2&creativeASIN=B07JGXM9WN&linkId=d6c2e2a3ce668c78d8766ee39aa93909" target="_blank">
+                                <div class="d-flex align-items-center">
+                                    <div class="mr-1 mr-sm-1 mr-md-1 mr-lg-2">
+                                        <span class="s-image"><img src="../iPhone-XR/apple-iphone-xr-silver.jpeg"></span>
+                                    </div>
+                                    <div class="d-flex flex-column ml-1">
+                                        <div class="flex-fill">Apple iPhone XR (64 GB)-White</div>
+                                        <div class="flex-fill">₹ 59,900</div>
+                                        <div class="flex-fill d-flex my-1">
+                                            <div class="mx-3 mx-sm-3 mx-md-1 mx-lg-4">
+                                                <span class="logo"><img src="../../../img/logo/amazon.jpg" alt="amazon-logo"></span>
+                                            </div>
+                                            <div class="ml-3 ml-sm-3 ml-md-1 ml-lg-4">
+                                                <span class="go-btn btn btn-warning d-inline">Buy Now</span>
+                                            </div>
+                                        </div>
+                                    </div>                                    
+                                </div>
+                            </a>
+                        </div>
+                        <div class="p-2 border rounded flex-fill store" style="background-image: url(light2.jpg)">
+                            <a href="https://www.amazon.in/gp/product/B072LPF91D/ref=as_li_tl?ie=UTF8&tag=skd0099-21&camp=3638&creative=24630&linkCode=as2&creativeASIN=B072LPF91D&linkId=5bf43473d4b9ba0b0a139f201e2dd622" target="_blank">
+                                <div class="d-flex align-items-center">
+                                    <div class="mr-1 mr-sm-1 mr-md-1 mr-lg-2">
+                                        <span class="s-image"><img src="../iPhone-X/apple-iphone-x-1.jpeg"></span>
+                                    </div>
+                                    <div class="d-flex flex-column ml-1">
+                                        <div class="flex-fill">Apple iPhone X (64 GB)-Space Grey</div>
+                                        <div class="flex-fill">₹ 68,999</div>
+                                        <div class="flex-fill d-flex my-1">
+                                            <div class="mx-3 mx-sm-3 mx-md-1 mx-lg-4">
+                                                <span class="logo"><img src="../../../img/logo/amazon.jpg" alt="amazon-logo"></span>
+                                            </div>
+                                            <div class="ml-3 ml-sm-3 ml-md-1 ml-lg-4">
+                                                <span class="go-btn btn btn-warning d-inline">Buy Now</span>
+                                            </div>
+                                        </div>
+                                    </div>                                    
+                                </div>
+                            </a>
+                        </div>
+                    </div>
+                </div>
---
+++ Mobile/Apple/more.php
@@ -0,0 +1,45 @@
+                <div class="similar border rounded mt-4">
+                    <h3 class="text-center font-weight-bold mb-0" style="background-color: #dfe6e9;">More From Apple</h3>
+                    <div class="d-flex flex-column border">
+                        <div class="d-flex my-2">
+                            <div class="flex-fill mx-2 slot">
+                                <a href="../iPhone-XS/iphone-xs.html">
+                                    <img src="../Img/ip-xs.png" alt="Apple iPhone XS">
+                                    <span class="d-block font-weight-bold text-center">iPhone XS</span>
+                                </a>
+                            </div>
+                            <div class="flex-fill mx-2 slot">
+                                <a href="../iPhone-XR/iphone-xr.html">
+                                    <img src="../Img/ip-xr.png" alt="Apple iPhone XR">
+                                    <span class="d-block font-weight-bold text-center">iPhone XR</span>
+                                </a>
+                            </div>
+                            <div class="flex-fill mx-2 slot">
+                                <a href="../iPhone-X/iphone-x.html">
+                                    <img src="../Img/ip-x.png" alt="Apple iPhone X">
+                                    <span class="d-block font-weight-bold text-center">iPhone X</span>
+                                </a>
+                            </div>
+                        </div>
+                        <div class="d-flex my-2">
+                            <div class="flex-fill mx-2 slot">
+                                <a href="../iPhone-8-Plus/iphone-8-plus.html">
+                                    <img src="../Img/ip-8plus.png" alt="Apple iPhone 8 Plus">
+                                    <span class="d-block font-weight-bold text-center">iPhone 8 Plus</span>
+                                </a>
+                            </div>
+                            <div class="flex-fill mx-2 slot">
+                                <a href="../iPhone-8/iphone-8.html">
+                                    <img src="../Img/ip-8.png" alt="Apple iPhone 8">
+                                    <span class="d-block font-weight-bold text-center">iPhone 8</span>
+                                </a>
+                            </div>
+                            <div class="flex-fill mx-2 slot">
+                                <a href="../iPhone-7-Plus/iphone-7-plus.html">
+                                    <img src="../Img/ip-7plus.png" alt="Apple iPhone 7 Plus">
+                                    <span class="d-block font-weight-bold text-center">iPhone 7 Plus</span>
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
